2a828cc62d9bba7e27e87ab9ecad7d3d99627e21
Rafael Mantovani <rafa-mantovani@hotmail.com>
2025-12-30 20:38:03 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rafaelvicencio/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.rafaelvicencio.workshopmongo.services;
+
+import com.rafaelvicencio.workshopmongo.domain.Post;
+import com.rafaelvicencio.workshopmongo.domain.User;
+import com.rafaelvicencio.workshopmongo.dto.UserDTO;
+import com.rafaelvicencio.workshopmongo.respository.PostRepository;
+import com.rafaelvicencio.workshopmongo.respository.UserRepository;
+import com.rafaelvicencio.workshopmongo.services.exception.ObjetctNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjetctNotFoundException("Objeto não encontrado"));
+    }
+}
