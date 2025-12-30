5e6ba677ab11c6c24897732f56c1e58a8a267cc7
Rafael Mantovani <rafa-mantovani@hotmail.com>
2025-12-30 19:50:20 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/rafaelvicencio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/rafaelvicencio/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.rafaelvicencio.workshopmongo.config;
 
 import com.rafaelvicencio.workshopmongo.domain.Post;
 import com.rafaelvicencio.workshopmongo.domain.User;
+import com.rafaelvicencio.workshopmongo.dto.AuthorDTO;
 import com.rafaelvicencio.workshopmongo.respository.PostRepository;
 import com.rafaelvicencio.workshopmongo.respository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
-
-import static java.util.Arrays.*;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -35,11 +34,10 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para sp abracos", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", maria);
-
-        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para sp abracos", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/rafaelvicencio/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/rafaelvicencio/workshopmongo/dto/AuthorDTO.java
@@ -4,22 +4,21 @@ import com.rafaelvicencio.workshopmongo.domain.User;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
-public class UserDTO implements Serializable {
+public class AuthorDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     private String id;
     private String name;
-    private String email;
 
-    private UserDTO() {
+    public AuthorDTO() {
     }
 
-    public UserDTO(User obj) {
+    public AuthorDTO(User obj) {
         id = obj.getId();
         name = obj.getName();
-        email = obj.getEmail();
     }
 
     public String getId() {
@@ -36,14 +35,6 @@ public class UserDTO implements Serializable {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
 }
